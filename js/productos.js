b55d5c0c6bea7274fbbb2ab1cae3d5e698dae264
Facundo Robert <robert.facundo@hotmail.com>
2024-04-27 13:54:02 -0300

    array de objetos y funcion de filtrados

--- js/productos.js
+++ js/productos.js
@@ -1,36 +1,44 @@
-//arrays de productos con clase constructora
-
 class Producto {
-    constructor(nombre, precio) {
+    constructor(nombre, precio, esComida) {
         this.nombre = nombre;
         this.precio = parseInt(precio, 10);
+        this.esComida = esComida; // True si es comida, False si es bebida
     }
 }
 
-
 const productosArray = [
-    new Producto("Pizza Margarita", "4000"),
-    new Producto("Pizza de prosciuto y hongos", "4300"),
-    new Producto("Pepperoni pizza", "4100"),
-    new Producto("Hamburguesa Doble Bacon", "6600"),
-    new Producto("Hamburguesa Doble Cheddar", "6900"),
-    new Producto("Hamburguesa Crispy Chicken", "6700"),
-    new Producto("Pasta con crema de hongos", "7900"),
-    new Producto("Empanadas de carne (1/2 docena)", "7000"),
-    new Producto("Empanadas de jamón y queso (1/2 docena)", "7000"),
-    new Producto("California Roll (12 rolls)", "8000"),
-    new Producto("Filadelfia Roll (12 rolls)", "8100"),
-    new Producto("Ramen", "7900"),
-    new Producto("Mirinda 2L", "3000"),
-    new Producto("Sprite 1.5L", "2500"),
-    new Producto("Coca-cola de 2.25L", "3100"),
-    new Producto("Agua Mineral de 2.25L", "3100")
+    new Producto("Pizza Margarita", "4000", true),
+    new Producto("Pizza de prosciuto y hongos", "4300", true),
+    new Producto("Pepperoni pizza", "4100", true),
+    new Producto("Hamburguesa Doble Bacon", "6600", true),
+    new Producto("Hamburguesa Doble Cheddar", "6900", true),
+    new Producto("Hamburguesa Crispy Chicken", "6700", true),
+    new Producto("Pasta con crema de hongos", "7900", true),
+    new Producto("Empanadas de carne (1/2 docena)", "7000", true),
+    new Producto("Empanadas de jamón y queso (1/2 docena)", "7000", true),
+    new Producto("California Roll (12 rolls)", "8000", true),
+    new Producto("Filadelfia Roll (12 rolls)", "8100", true),
+    new Producto("Ramen", "7900", true),
+    new Producto("Mirinda 2L", "3000", false),
+    new Producto("Sprite 1.5L", "2500", false),
+    new Producto("Coca-cola de 2.25L", "3100", false),
+    new Producto("Agua Mineral de 2.25L", "3100", false)
 ];
 
 productosArray.forEach(producto => {
     console.log(`Producto: ${producto.nombre}, Precio: $${producto.precio}`);
 });
 
+
+function filtrarPorTipo(esComida) {
+    return productosArray.filter(producto => producto.esComida === esComida);
+}
+
+const comidas = filtrarPorTipo(true);
+console.log("Comidas:", comidas);
+
+const bebidas = filtrarPorTipo(false);
+console.log("Bebidas:", bebidas);
 
 
 
