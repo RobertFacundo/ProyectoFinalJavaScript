e8a00f0bf56373f3b206ae6efee0346be9413aa3
Facundo Robert <robert.facundo@hotmail.com>
2024-05-28 10:08:02 -0300

    Proyecto final

--- js/display.js
+++ js/display.js
@@ -1,35 +1,4 @@
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     const gridButton = document.getElementById('grid');
-//     const listButton = document.getElementById('lista');
-//     const productsContainer = document.querySelector('.contenedor-productos');
-
-    
-//     function displayGrid() {
-//         productsContainer.classList.remove('lista');
-//         gridButton.classList.add('active');
-//         listButton.classList.remove('active');
-        
-//         localStorage.setItem('vista', 'grid');
-//     }
-
-   
-//     function displayList() {
-//         productsContainer.classList.add('lista');
-//         listButton.classList.add('active');
-//         gridButton.classList.remove('active');
-    
-//         localStorage.setItem('vista', 'lista');
-//     }
-
-//     gridButton.addEventListener('click', displayGrid);
-//     listButton.addEventListener('click', displayList);
-
-   
-//     const eleccionPrevia = localStorage.getItem('vista');
-
-//     eleccionPrevia === 'lista' ? displayList() : displayGrid();
-// });
 
 document.addEventListener('DOMContentLoaded', function() {
     const gridButton = document.getElementById('grid');
--- js/productos.js
+++ js/productos.js
@@ -1,5 +1,6 @@
 class Producto {
-    constructor(nombre, precio, esComida, img, desc) {
+    constructor(id, nombre, precio, esComida, img, desc) {
+        this.id = id;
         this.nombre = nombre;
         this.precio = parseInt(precio, 10);
         this.esComida = esComida;
@@ -8,28 +9,25 @@ class Producto {
     }
 }
 
-const productosArray = [
-    new Producto("Pizza Margarita", "4000", true, "./multimedia/cards/pizzamargarita.jpg", "Clásica italiana que lleva salsa de tomate, mozzarella fresca y hojas de albahaca fresca"),
-    new Producto("Pizza de prosciuto y hongos", "4300", true, "./multimedia/cards/Pizza-prosciutto-e-funghi.jpg", "Combinación de salsa de tomate, queso mozzarella, jamón (prosciutto) y champiñones"),
-    new Producto("Pepperoni pizza", "4100", true, "./multimedia/cards/pepperoni.jpg", "Esta pizza lleva salsa de tomate, queso mozzarella y rodajas de pepperoni (salami picante)"),
-    new Producto("Hamburguesa Doble Bacon", "6600", true, "./multimedia/cards/HamburguesaDobleBacon.png", "Dos medallones de carne vacuna, queso cheddar fundido y en fetas, bacon crujiente y cebolla en trozos"),
-    new Producto("Hamburguesa Doble Cheddar", "6900", true, "./multimedia/cards/cheddarbacon.png", "Clásica hamburguesa con queso cheddar en feta y fundido, dos tiras de bacon enteras, cebolla grillada en nuestro inigualable pan con semillas de sesamo"),
-    new Producto("Hamburguesa Crispy Chicken", "6700", true, "./multimedia/cards/crispychicken.png", "Pollo sazonado y crujiente, cubierto con obligatorio queso derretido, apilado sobre panes suaves con cebolla, aguacate, lechuga, tomate y mayonesa de ajo"),
-    new Producto("Pasta con crema de hongos", "7900", true, "./multimedia/cards/Espaguetis-con-hongos.jpg", "Plato de fetuccini con crema de hongos portobello"),
-    new Producto("Empanadas de carne (1/2 docena)", "7000", true, "./multimedia/cards/empanadas-de-carne-cortada-a-cuchillo-4.jpg", "Riquísimas empanadas con carne cortada a cuchillo"),
-    new Producto("Empanadas de jamón y queso (1/2 docena)", "7000", true, "./multimedia/cards/empanadajamonyqueso.jpg", "Sabrosas empanadas de jamón y queso"),
-    new Producto("California Roll (12 rolls)", "8000", true, "./multimedia/cards/CALIFORNIA-roll.jpg", "Arroz, alga nori, aguacate, pepino, surimi, sésamo"),
-    new Producto("Filadelfia Roll (12 rolls)", "8100", true, "./multimedia/cards/FILADELFIAroll.jpg", "Arroz, alga nori, aguacate, pepino, salmón, queso crema, sésamo"),
-    new Producto("Ramen", "7900", true, "./multimedia/cards/ramen.jpeg", "Caldo de cerdo en cocción lenta, miso, noodles, huevo poche, bondiola braseada, verdeo, boniato crocante, alga kombu, pickles de cebolla morada, sriracha"),
-    new Producto("Mirinda 2L", "3000", false, "./multimedia/cards/mirinda.png",''),
-    new Producto("Sprite 1.5L", "2500", false, "./multimedia/cards/sprite.png",''),
-    new Producto("Coca-cola de 2.25L", "3100", false, "./multimedia/cards/cocacola.png",''),
-    new Producto("Agua Mineral de 2.25L", "3100", false, "./multimedia/cards/agua.png",'')
-];
+let productosArray = [];
+
+fetch('./data/productos.json')
+    .then(response => response.json())
+    .then(data => {
+        productosArray = data.map(producto => new Producto(
+            producto.id, producto.nombre, producto.precio,
+            producto.esComida, producto.img, producto.desc
+        ));
+
+        const productosJSON = JSON.stringify(productosArray);
+        localStorage.setItem('productos', productosJSON);
+
+        bebidasComidas();
+    })
+
+    .catch(error => console.error('error al cargar productos', error));
 
 
-const productosJSON = JSON.stringify(productosArray);
-localStorage.setItem('productos', productosJSON);
 
 const contenedorProductos = document.getElementById('contenedor-productos');
 
@@ -46,7 +44,7 @@ function bebidasComidas() {
            <section class=texto> <h3>${producto.nombre}</h3>
             <p>${producto.desc}</p>
             <p>Precio: <b>$${producto.precio}</b></p></section>
-            <button> <span>Agregar pedido</span></button>
+            <button class="agregar-al-carrito"> <span class="agregar-al-carrito">Agregar pedido</span></button>
         `;
         contenedorProductos.appendChild(div);
     });
@@ -55,49 +53,227 @@ function bebidasComidas() {
 bebidasComidas();
 
 
+document.addEventListener('DOMContentLoaded', function () {
+    function productosFiltrados(esComida) {
 
-function productosFiltrados(esComida) {
+        const productosFiltrados = productosArray.filter(producto => producto.esComida === esComida);
 
-    const productosFiltrados = productosArray.filter(producto => producto.esComida === esComida);
+        contenedorProductos.innerHTML = '';
 
-    contenedorProductos.innerHTML = '';
-
-    productosFiltrados.forEach(producto => {
-        const div = document.createElement('div');
-        div.classList.add('card');
-        div.innerHTML = `
+        productosFiltrados.forEach(producto => {
+            const div = document.createElement('div');
+            div.classList.add('card');
+            div.innerHTML = `
             <img src="${producto.img}" alt="${producto.nombre}">
             <section class=texto>
             <h3>${producto.nombre}</h3>
             <p>${producto.desc}</p>
-            <p>Precio: $${producto.precio}</p> </section>
-            <button> <span>Agregar pedido</span></button>
+            <p>Precio: <b>$${producto.precio}</b></p> </section>
+            <button class="agregar-al-carrito"> <span>Agregar pedido</span></button>
         `;
-        contenedorProductos.appendChild(div);
+            contenedorProductos.appendChild(div);
+        });
+
+        esComida ? localStorage.setItem('ultimaEleccion', 'comidas') : localStorage.setItem('ultimaEleccion', 'bebidas');
+    }
+
+    const byc = document.getElementById('byc');
+    const bebidas = document.getElementById('be');
+    const comidas = document.getElementById('co');
+
+
+
+    byc.addEventListener('click', () => bebidasComidas());
+
+
+    bebidas.addEventListener('click', () => productosFiltrados(false));
+
+
+    comidas.addEventListener('click', () => productosFiltrados(true));
+
+
+    const ultimaEleccion = localStorage.getItem('ultimaEleccion');
+
+
+    ultimaEleccion === 'comidas' || ultimaEleccion === 'bebidas' ? productosFiltrados(ultimaEleccion === 'comidas') : bebidasComidas();
+
+    const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+    const carritoBody = document.getElementById('carrito-body');
+    const totalElement = document.getElementById('total');
+
+
+    function actualizarCarrito() {
+        carritoBody.innerHTML = '';
+        let total = 0;
+
+        carrito.forEach((producto, index) => {
+            const subtotal = producto.precio * producto.cantidad;
+            total += subtotal;
+
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+            <td>${producto.nombre}</td>
+            <td>$${producto.precio}</td>
+            <td>${producto.cantidad}</td>
+            <td>$${subtotal}</td>
+            <td>
+                <button><i data-index="${index}" class="fa-solid fa-xmark eliminar-producto"></i></button>
+            </td>
+        `;
+            carritoBody.appendChild(tr);
+        });
+
+        totalElement.textContent = `$${total}`;
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+    }
+
+    document.addEventListener('click', (e) => {
+        if (e.target.classList.contains('agregar-al-carrito')) {
+            const productoDiv = e.target.closest('.card');
+            const productoId = productoDiv.querySelector('img').alt;
+
+            const producto = productosArray.find(p => p.nombre === productoId);
+            const productoEnCarrito = carrito.find(p => p.id === producto.id);
+
+            if (productoEnCarrito) {
+                productoEnCarrito.cantidad++;
+            } else {
+                carrito.push({ ...producto, cantidad: 1 });
+            }
+
+            actualizarCarrito();
+
+            Swal.fire({
+                icon: 'success',
+                title: 'Producto añadido',
+                text: `${producto.nombre} ha sido añadido al carrito.`,
+                showConfirmButton: false,
+                timer: 1300,
+                customClass: {
+                    popup: 'my-swal-font',
+                    title: 'my-swal-font',
+                    content: 'my-swal-font',
+                    confirmButton: 'my-swal-font'
+                }
+            });
+        }
+
+        if (e.target.classList.contains('eliminar-producto')) {
+            const index = e.target.dataset.index;
+            carrito.splice(index, 1);
+            actualizarCarrito();
+        }
+
+        if (e.target.id === 'vaciar-carrito') {
+            if (carrito.length > 0) {
+                Swal.fire({
+                    title: '¿Estás seguro?',
+                    icon: 'warning',
+                    showCancelButton: true,
+                    confirmButtonColor: '#2CCD46',
+                    cancelButtonColor: '#1c6907',
+                    confirmButtonText: 'Sí, vaciar carrito',
+                    cancelButtonText: 'Cancelar',
+                    customClass: {
+                        popup: 'my-swal-font',
+                        title: 'my-swal-font',
+                        content: 'my-swal-font',
+                        confirmButton: 'my-swal-font',
+                        cancelButton: 'my-swal-font'
+                    }
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        carrito.length = 0;
+                        actualizarCarrito();
+                        Swal.fire({
+                            icon: 'success',
+                            title: '¡Carrito vaciado!',
+                            text: 'Tu carrito ha sido vaciado.',
+                            showConfirmButton: true,
+                            confirmButtonColor: '#2CCD46',
+                            customClass: {
+                                popup: 'my-swal-font',
+                                title: 'my-swal-font',
+                                content: 'my-swal-font',
+                                confirmButton: 'my-swal-font'
+                            }
+                        });
+                    }
+                }).catch((error) => {
+                    console.error('Error vaciando el carrito:', error);
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Error',
+                        text: 'Hubo un problema vaciando el carrito. Por favor, intenta de nuevo.',
+                        showConfirmButton: true,
+                        confirmButtonColor: '#2CCD46',
+                        customClass: {
+                            popup: 'my-swal-font',
+                            title: 'my-swal-font',
+                            content: 'my-swal-font',
+                            confirmButton: 'my-swal-font'
+                        }
+                    });
+                });
+            } else {
+                Swal.fire({
+                    icon: 'info',
+                    title: 'Carrito vacío',
+                    text: 'No hay productos en el carrito para vaciar.',
+                    showConfirmButton: false,
+                    timer: 1900,
+                    customClass: {
+                        popup: 'my-swal-font',
+                        title: 'my-swal-font',
+                        content: 'my-swal-font',
+                        confirmButton: 'my-swal-font'
+                    }
+                });
+            }
+        }
+
+        if (e.target.id === 'realizar-pedido') {
+            if (carrito.length > 0) {
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Pedido realizado',
+                    text: '¡Tu pedido ha sido realizado con éxito!',
+                    showConfirmButton: true,
+                    confirmButtonColor: '#2CCD46',
+                    customClass: {
+                        popup: 'my-swal-font',
+                        title: 'my-swal-font',
+                        content: 'my-swal-font',
+                        confirmButton: 'my-swal-font'
+                    }
+                });
+                carrito.length = 0;
+                actualizarCarrito();
+            } else {
+                Swal.fire({
+                    icon: 'info',
+                    title: 'Carrito vacío',
+                    text: 'Agrega productos al carrito para poder confirmar la compra',
+                    showConfirmButton: false,
+                    timer: 1900,
+                    customClass: {
+                        popup: 'my-swal-font',
+                        title: 'my-swal-font',
+                        content: 'my-swal-font',
+                        confirmButton: 'my-swal-font'
+                    }
+                });
+            }
+        }
     });
 
-    esComida ? localStorage.setItem('ultimaEleccion', 'comidas') : localStorage.setItem('ultimaEleccion', 'bebidas');
-}
+    actualizarCarrito();
 
-const byc = document.getElementById('byc');
-const bebidas = document.getElementById('be');
-const comidas = document.getElementById('co');
+});
 
 
 
-byc.addEventListener('click', () => bebidasComidas());
 
-
-bebidas.addEventListener('click', () => productosFiltrados(false));
-
-
-comidas.addEventListener('click', () => productosFiltrados(true));
-
-
-const ultimaEleccion = localStorage.getItem('ultimaEleccion');
-
-
-ultimaEleccion === 'comidas' || ultimaEleccion === 'bebidas' ? productosFiltrados(ultimaEleccion === 'comidas') : bebidasComidas();
 
 
 
